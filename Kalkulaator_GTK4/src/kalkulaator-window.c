652afa566cb60b88e03c1fe3388947f288b8e418
MiniHarambi <43966304+MiniHarambi@users.noreply.github.com>
2024-03-17 15:50:06 +0200

    Update kalkulaator-window.c

--- Kalkulaator_GTK4/src/kalkulaator-window.c
+++ Kalkulaator_GTK4/src/kalkulaator-window.c
@@ -71,6 +71,11 @@ static void kalkulaator_window__nr_clicked(GtkButton *button, gpointer user_data
     KalkulaatorWindow *window = KALKULAATOR_WINDOW(user_data);
     const gchar* text = gtk_button_get_label(button);
 
+    const gchar *current_text = gtk_editable_get_text(GTK_EDITABLE(window->display));
+    gchar *updated_text = g_strconcat(current_text, text, NULL);
+    gtk_editable_set_text(GTK_EDITABLE(window->display), updated_text);
+    g_free(updated_text);
+    /*
     gchar *updated_expression = g_strdup_printf("%s%s", window->expression, text);
     g_free(window->expression);
     window->expression = updated_expression;
@@ -78,6 +83,7 @@ static void kalkulaator_window__nr_clicked(GtkButton *button, gpointer user_data
     if (window->expression != NULL) {
         gtk_editable_set_text(GTK_EDITABLE(window->display), window->expression);
     }
+     */
 }
 
 
@@ -88,7 +94,7 @@ static void kalkulaator_window__clear_clicked(GtkButton *button, gpointer user_d
     // Clear the expression
     g_free(window->expression);
     window->expression = g_strdup("");
-g_print("clear button");
+    g_print("clear button");
     // Update the display
     gtk_editable_set_text(GTK_EDITABLE(window->display), "");
 }
@@ -96,30 +102,30 @@ g_print("clear button");
 
 static void kalkulaator_window__delete_clicked(GtkButton *button, gpointer user_data)
 {
-  KalkulaatorWindow *window = KALKULAATOR_WINDOW(user_data);
+    KalkulaatorWindow *window = KALKULAATOR_WINDOW(user_data);
 
-  // Delete the last character from the expression
-  size_t len = strlen(window->expression);
-  if (len > 0) {
-    window->expression[len - 1] = '\0';
-g_print("delete button");
-    // Update the display
-    gtk_editable_set_text(GTK_EDITABLE(window->display), window->expression);
-  }
+    // Get the current text from the display widget
+    const gchar *current_text = gtk_editable_get_text(GTK_EDITABLE(window->display));
+
+    // Delete the last character from the current text
+    size_t len = strlen(current_text);
+    if (len > 0) {
+        gchar *updated_text = g_strndup(current_text, len - 1);
+        // Update the display
+        gtk_editable_set_text(GTK_EDITABLE(window->display), updated_text);
+        g_free(updated_text);
+    }
 }
-
 static void kalkulaator_window__dot_clicked(GtkButton *button, gpointer user_data)
 {
   KalkulaatorWindow *window = KALKULAATOR_WINDOW(user_data);
 
   // Append a dot to the expression
   gchar *dot = ".";
-  gchar *updated_expression = g_strconcat(window->expression, dot, NULL);
-  g_free(window->expression);
-  window->expression = updated_expression;
-g_print(". button");
-  // Update the display
-  gtk_editable_set_text(GTK_EDITABLE(window->display), window->expression);
+   const gchar *current_text = gtk_editable_get_text(GTK_EDITABLE(window->display));
+    gchar *updated_text = g_strconcat(current_text, dot, NULL);
+    gtk_editable_set_text(GTK_EDITABLE(window->display), updated_text);
+    g_free(updated_text);
 }
 
 static void kalkulaator_window__add_clicked(GtkButton *button, gpointer user_data)
@@ -128,12 +134,10 @@ static void kalkulaator_window__add_clicked(GtkButton *button, gpointer user_dat
 
   // Append a plus sign to the expression
   gchar *add = "+";
-  gchar *updated_expression = g_strconcat(window->expression, add, NULL);
-  g_free(window->expression);
-  window->expression = updated_expression;
-g_print("+ button");
-  // Update the display
-  gtk_editable_set_text(GTK_EDITABLE(window->display), window->expression);
+   const gchar *current_text = gtk_editable_get_text(GTK_EDITABLE(window->display));
+    gchar *updated_text = g_strconcat(current_text, add, NULL);
+    gtk_editable_set_text(GTK_EDITABLE(window->display), updated_text);
+    g_free(updated_text);
 }
 
 static void kalkulaator_window__subtract_clicked(GtkButton *button, gpointer user_data)
@@ -142,12 +146,10 @@ static void kalkulaator_window__subtract_clicked(GtkButton *button, gpointer use
 
   // Append a minus sign to the expression
   gchar *subtract = "-";
-  gchar *updated_expression = g_strconcat(window->expression, subtract, NULL);
-  g_free(window->expression);
-  window->expression = updated_expression;
-g_print("- button");
-  // Update the display
-  gtk_editable_set_text(GTK_EDITABLE(window->display), window->expression);
+   const gchar *current_text = gtk_editable_get_text(GTK_EDITABLE(window->display));
+    gchar *updated_text = g_strconcat(current_text, subtract, NULL);
+    gtk_editable_set_text(GTK_EDITABLE(window->display), updated_text);
+    g_free(updated_text);
 }
 
 static void kalkulaator_window__multiply_clicked(GtkButton *button, gpointer user_data)
@@ -156,12 +158,10 @@ static void kalkulaator_window__multiply_clicked(GtkButton *button, gpointer use
 
   // Append a multiplication sign to the expression
   gchar *multiply = "*";
-  gchar *updated_expression = g_strconcat(window->expression, multiply, NULL);
-  g_free(window->expression);
-  window->expression = updated_expression;
-g_print("* button");
-  // Update the display
-  gtk_editable_set_text(GTK_EDITABLE(window->display), window->expression);
+  const gchar *current_text = gtk_editable_get_text(GTK_EDITABLE(window->display));
+    gchar *updated_text = g_strconcat(current_text, multiply, NULL);
+    gtk_editable_set_text(GTK_EDITABLE(window->display), updated_text);
+    g_free(updated_text);
 }
 
 static void kalkulaator_window__divide_clicked(GtkButton *button, gpointer user_data)
@@ -170,12 +170,10 @@ static void kalkulaator_window__divide_clicked(GtkButton *button, gpointer user_
 
   // Append a division sign to the expression
   gchar *divide = "/";
-  gchar *updated_expression = g_strconcat(window->expression, divide, NULL);
-  g_free(window->expression);
-  window->expression = updated_expression;
-g_print("/ button");
-  // Update the display
-  gtk_editable_set_text(GTK_EDITABLE(window->display), window->expression);
+   const gchar *current_text = gtk_editable_get_text(GTK_EDITABLE(window->display));
+    gchar *updated_text = g_strconcat(current_text, divide, NULL);
+    gtk_editable_set_text(GTK_EDITABLE(window->display), updated_text);
+    g_free(updated_text);
 }
 
 static void kalkulaator_window__sqrt_clicked(GtkButton *button, gpointer user_data)
@@ -184,12 +182,10 @@ static void kalkulaator_window__sqrt_clicked(GtkButton *button, gpointer user_da
 
   // Append the sqrt function to the expression
   gchar *sqrt = "sqrt(";
-  gchar *updated_expression = g_strconcat(window->expression, sqrt, NULL);
-  g_free(window->expression);
-  window->expression = updated_expression;
-g_print("sqrt button");
-  // Update the display
-  gtk_editable_set_text(GTK_EDITABLE(window->display), window->expression);
+   const gchar *current_text = gtk_editable_get_text(GTK_EDITABLE(window->display));
+    gchar *updated_text = g_strconcat(current_text, sqrt, NULL);
+    gtk_editable_set_text(GTK_EDITABLE(window->display), updated_text);
+    g_free(updated_text);
 }
 
 static void kalkulaator_window__square_clicked(GtkButton *button,gpointer  user_data)
@@ -198,12 +194,10 @@ static void kalkulaator_window__square_clicked(GtkButton *button,gpointer  user_
 
   // Append the square function to the expression
   gchar *pow = "^";
-  gchar *updated_expression = g_strconcat(window->expression, pow, NULL);
-  g_free(window->expression);
-  window->expression = updated_expression;
-g_print("square button");
-  // Update the display
-  gtk_editable_set_text(GTK_EDITABLE(window->display), window->expression);
+   const gchar *current_text = gtk_editable_get_text(GTK_EDITABLE(window->display));
+    gchar *updated_text = g_strconcat(current_text, pow, NULL);
+    gtk_editable_set_text(GTK_EDITABLE(window->display), updated_text);
+    g_free(updated_text);
 }
 
 static void kalkulaator_window__history_clicked(GtkButton *button, gpointer user_data)
@@ -221,31 +215,26 @@ static void kalkulaator_window__ans_clicked(GtkButton *button, gpointer user_dat
     // Check if there is a previous result and it's a valid number
     if (window->last_result != NULL && g_ascii_isdigit(window->last_result[0]))
     {
+        // Get the text from the display widget
+        const gchar *current_text = gtk_editable_get_text(GTK_EDITABLE(window->display));
+
         // Convert the last result to a numerical value
         double ans = atof(window->last_result);
 
         // Convert the numerical value back to string
         gchar *ans_string = g_strdup_printf("%g", ans);
 
-        // Append the numerical value to the expression
-        gchar *updated_expression = g_strconcat(window->expression, ans_string, NULL);
-
-        // Free the previous expression
-        g_free(window->expression);
-
-        // Update the expression with the new one
-        window->expression = updated_expression;
+        // Concatenate the numerical value to the current text
+        gchar *updated_text = g_strconcat(current_text, ans_string, NULL);
 
         // Update the display with the new expression
-        gtk_editable_set_text(GTK_EDITABLE(window->display), window->expression);
+        gtk_editable_set_text(GTK_EDITABLE(window->display), updated_text);
 
         // Free the string representation of the numerical value
         g_free(ans_string);
+        g_free(updated_text);
     }
 }
-
-
-
 
 static void kalkulaator_window__sulg1_clicked(GtkButton *button, gpointer user_data)
 {
@@ -253,25 +242,21 @@ static void kalkulaator_window__sulg1_clicked(GtkButton *button, gpointer user_d
 
   // Append an opening parenthesis to the expression
   gchar *parenthesis_open = "(";
-  gchar *updated_expression = g_strconcat(window->expression, parenthesis_open, NULL);
-  g_free(window->expression);
-  window->expression = updated_expression;
-g_print("( button");
-  // Update the display
-  gtk_editable_set_text(GTK_EDITABLE(window->display), window->expression);
+   const gchar *current_text = gtk_editable_get_text(GTK_EDITABLE(window->display));
+    gchar *updated_text = g_strconcat(current_text, parenthesis_open, NULL);
+    gtk_editable_set_text(GTK_EDITABLE(window->display), updated_text);
+    g_free(updated_text);
 }
 static void kalkulaator_window__sulg2_clicked(GtkButton *button, gpointer user_data)
 {
   KalkulaatorWindow *window = KALKULAATOR_WINDOW(user_data);
 
   // Append an opening parenthesis to the expression
-  gchar *parenthesis_open = ")";
-  gchar *updated_expression = g_strconcat(window->expression, parenthesis_open, NULL);
-  g_free(window->expression);
-  window->expression = updated_expression;
-  g_print(") button");
-  // Update the display
-  gtk_editable_set_text(GTK_EDITABLE(window->display), window->expression);
+  gchar *parenthesis_close = ")";
+  const gchar *current_text = gtk_editable_get_text(GTK_EDITABLE(window->display));
+    gchar *updated_text = g_strconcat(current_text, parenthesis_close, NULL);
+    gtk_editable_set_text(GTK_EDITABLE(window->display), updated_text);
+    g_free(updated_text);
 }
 
 
@@ -279,8 +264,10 @@ static void kalkulaator_window__equal_clicked(GtkButton *button, gpointer user_d
 {
     KalkulaatorWindow *window = KALKULAATOR_WINDOW(user_data);
 
-    // Get the text from the entry widget
+    // Get the text from the display widget
     const gchar *text = gtk_editable_get_text(GTK_EDITABLE(window->display));
+
+    // Calculate the result
     gchar *result = CalcMain((char*)text);
 
     // Store the result as the last result
@@ -290,12 +277,6 @@ static void kalkulaator_window__equal_clicked(GtkButton *button, gpointer user_d
     // Print the result on the display
     gtk_editable_set_text(GTK_EDITABLE(window->display), result);
     g_free(result);
-
-    // Clear the expression if it's not already empty
-    if (strlen(window->expression) > 0) {
-        g_free(window->expression);
-        window->expression = g_strdup("");
-    }
 }
 
 
@@ -305,12 +286,10 @@ static void kalkulaator_window__log_clicked(GtkButton *button, gpointer user_dat
 
   // Append the square function to the expression
   gchar *log = "log(";
-  gchar *updated_expression = g_strconcat(window->expression, log, NULL);
-  g_free(window->expression);
-  window->expression = updated_expression;
-g_print("log button");
-  // Update the display
-  gtk_editable_set_text(GTK_EDITABLE(window->display), window->expression);
+   const gchar *current_text = gtk_editable_get_text(GTK_EDITABLE(window->display));
+    gchar *updated_text = g_strconcat(current_text, log, NULL);
+    gtk_editable_set_text(GTK_EDITABLE(window->display), updated_text);
+    g_free(updated_text);
 }
 
 static void kalkulaator_window_class_init (KalkulaatorWindowClass *klass)
@@ -350,7 +329,7 @@ kalkulaator_window_init (KalkulaatorWindow *self)
 
   g_print("initializing display widget: %p\n", (void*) self->display);
 
-  self->expression = g_strdup("");
+  //self->display = g_strdup("");
 
   g_signal_connect(self->button0, "clicked", G_CALLBACK(kalkulaator_window__nr_clicked), self);
   g_signal_connect(self->button1, "clicked", G_CALLBACK(kalkulaator_window__nr_clicked), self);
